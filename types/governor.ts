73ba30e78b455ff80eaa7727315823774c009052
Alexander Evchenko <alexivchenko9@gmail.com>
2024-06-27 11:49:59 +0400

    type: add typing fo Governor

---
+++ types/governor.ts
@@ -0,0 +1,20 @@
+import { BigNumberish, BytesLike } from 'ethers'
+
+export enum ProposalState {
+  Pending,
+  Active,
+  Canceled,
+  Defeated,
+  Succeeded,
+  Queued,
+  Expired,
+  Executed,
+}
+
+export enum VoteType {
+  Against,
+  For,
+  Abstain,
+}
+
+export type Proposal = [string[], BigNumberish[], BytesLike[]]
