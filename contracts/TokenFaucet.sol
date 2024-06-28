e876e09ed0fb432da2925da18dd9dc4bb909cf63
Alex <shenshin@me.com>
2024-06-28 21:02:26 +0200

    fix: slither findings

--- contracts/TokenFaucet.sol
+++ contracts/TokenFaucet.sol
@@ -5,49 +5,48 @@
  * https://github.com/riflabs/rif-faucet/blob/master/contracts/TokenFaucet.sol
  */
 
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity ^0.8.20;
 
-import "./RIFToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenFaucet {
-    address public owner;
-    RIFToken public tokenContract;
+contract TokenFaucet is Ownable {
+  IERC20 public immutable tokenContract;
 
-    mapping(address => uint) cannotDispenseUntil;
+  mapping(address => uint) cannotDispenseUntil;
 
-    uint public dispenseValue = 10 * 10 ** 18;
-    uint public dispenceFrequency = 1 hours;
+  uint256 public dispenseValue = 10 * 10 ** 18; // 10 tRIFs
+  uint256 public dispenceFrequency = 1 hours;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+  event DispenceFrequencyChanged(address changer, uint256 oldValue, uint256 newValue);
+  event DispenceValueChanged(address changer, uint256 oldValue, uint256 newValue);
 
-    modifier canDispense(address to) {
-        require(cannotDispenseUntil[to] < now, "CANNOT DISPENSE MORE THAN 1 TIME PER HOUR");
-        _;
-    }
+  modifier canDispense(address to) {
+    require(cannotDispenseUntil[to] < block.timestamp, "CANNOT DISPENSE MORE THAN 1 TIME PER HOUR");
+    _;
+  }
 
-    constructor(RIFToken _tokenContract) public {
-        owner = msg.sender;
-        tokenContract = _tokenContract;
-    }
+  constructor(IERC20 rifToken) Ownable(msg.sender) {
+    tokenContract = rifToken;
+  }
 
-    function recover() public {
-        uint totalAmount = tokenContract.balanceOf(address(this));
-        tokenContract.transfer(owner, totalAmount);
-    }
+  function recover() public returns (bool) {
+    uint256 totalAmount = tokenContract.balanceOf(address(this));
+    return tokenContract.transfer(owner(), totalAmount);
+  }
 
-    function dispense(address to) public canDispense(to) {
-        tokenContract.transfer(to, dispenseValue);
-        cannotDispenseUntil[to] = now + dispenceFrequency;
-    }
+  function dispense(address to) public canDispense(to) returns (bool) {
+    cannotDispenseUntil[to] = block.timestamp + dispenceFrequency;
+    return tokenContract.transfer(to, dispenseValue);
+  }
 
-    function setDispenseValue(uint value) public onlyOwner {
-        dispenseValue = value;
-    }
+  function setDispenseValue(uint256 value) public onlyOwner {
+    emit DispenceValueChanged(msg.sender, dispenseValue, value);
+    dispenseValue = value;
+  }
 
-    function setDispenseFrequency(uint _seconds) public onlyOwner {
-        dispenceFrequency = _seconds;
-    }
+  function setDispenseFrequency(uint256 freqSeconds) public onlyOwner {
+    emit DispenceFrequencyChanged(msg.sender, dispenceFrequency, freqSeconds);
+    dispenceFrequency = freqSeconds;
+  }
 }
