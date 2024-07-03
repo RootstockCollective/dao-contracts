5172d2e6cdbfff70c49c65102c80b01e4807059b
Alex <shenshin@me.com>
2024-07-03 21:16:08 +0200

    fix: assign roles to governor on timelock

--- scripts/deploy-governor.ts
+++ scripts/deploy-governor.ts
@@ -1,29 +1,42 @@
 import hre, { ethers, upgrades } from 'hardhat'
-import { GovernorTimelockControlUpgradeable, RootDao } from '../typechain-types'
+import { RootDao } from '../typechain-types'
 
-export const deployGovernor = async (tokenAddress: string, deployerAddress: string) => {
+export const deployGovernor = async (
+  tokenAddress: string,
+  deployerAddress: string,
+  timelockAddress: string,
+) => {
+  const timelock = await hre.ethers.getContractAt('DaoTimelockUpgradable', timelockAddress)
   const RootDAOFactory = await ethers.getContractFactory('RootDao')
-  const TimelockFactory = await ethers.getContractFactory('DaoTimelockUpgradable')
-  // TODO: figure out why it allows to put only a single argument
-  const timelock = (await upgrades.deployProxy(TimelockFactory, [1, [], [], ethers.ZeroAddress], {
-    initializer: 'initialize(uint256,address[],address[],address)',
-    kind: 'uups',
-    timeout: 0,
-  })) as unknown as GovernorTimelockControlUpgradeable
-
   const rootDAOGovernor = (await upgrades.deployProxy(
     RootDAOFactory,
-    [tokenAddress, await timelock.getAddress(), deployerAddress],
+    [tokenAddress, timelockAddress, deployerAddress],
     {
       initializer: 'initialize',
       kind: 'uups',
       timeout: 0,
     },
   )) as unknown as RootDao
+  await rootDAOGovernor.waitForDeployment()
+  const governorAddr = await rootDAOGovernor.getAddress()
 
-  const rootDAOContact = await rootDAOGovernor.waitForDeployment()
+  // grant Proposer role to the Governor
+  const proposerRole = await timelock.PROPOSER_ROLE()
+  const grantProposerTx = await timelock.grantRole(proposerRole, governorAddr)
+  await grantProposerTx.wait()
 
-  console.log(`Deployed Governor on ${hre.network.name} with address ${await rootDAOContact.getAddress()}`)
+  // grant Executor role to the Governor
+  const executorRole = await timelock.EXECUTOR_ROLE()
+  const grantExecutorRoleTx = await timelock.grantRole(executorRole, governorAddr)
+  await grantExecutorRoleTx.wait()
 
-  return rootDAOContact
+  // renounce Admin role - this operation should be finally done by the DAO deployer
+  const adminRole = await timelock.DEFAULT_ADMIN_ROLE()
+  const [deployer] = await hre.ethers.getSigners()
+  const renounceTx = await timelock.renounceRole(adminRole, deployer.address)
+  await renounceTx.wait()
+
+  console.log(`Deployed Governor on ${hre.network.name} with address ${await rootDAOGovernor.getAddress()}`)
+
+  return rootDAOGovernor
 }
