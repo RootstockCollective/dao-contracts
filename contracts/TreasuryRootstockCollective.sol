087bb412ac382f2efd3aef069a71a6d41f89af7b
Alex <shenshin@me.com>
2024-09-13 23:04:07 +0200

    fix: change ITreasury interface name to ITreasuryRootstockCollective

--- contracts/TreasuryRootstockCollective.sol
+++ contracts/TreasuryRootstockCollective.sol
@@ -5,11 +5,11 @@ pragma solidity ^0.8.20;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {ITreasury} from "./ITreasury.sol";
+import {ITreasuryRootstockCollective} from "./ITreasuryRootstockCollective.sol";
 
 using SafeERC20 for IERC20;
 
-contract TreasuryRootstockCollective is AccessControl, ReentrancyGuard, ITreasury {
+contract TreasuryRootstockCollective is AccessControl, ReentrancyGuard, ITreasuryRootstockCollective {
   event TokenWhitelisted(address indexed token);
 
   event TokenUnwhitelisted(address indexed token);
