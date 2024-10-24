35f9da774aa0c2807a3608b63248f7454e9f33c6
Alex <shenshin@me.com>
2024-10-24 03:18:36 +0200

    feat: airdrop hardhat task

--- contracts/NFT/ERC721UpgradableAirdroppable.sol
+++ contracts/NFT/ERC721UpgradableAirdroppable.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 
 import {ERC721UpgradableBase} from "./ERC721UpgradableBase.sol";
 
-struct AirdropItem {
+struct AirdropRecipient {
   address receiver;
   string ipfsCid;
 }
@@ -12,12 +12,16 @@ struct AirdroppableStorage {
   uint256 _nextTokenId;
 }
 
-abstract contract ERC721UpgradableAirdroppable is ERC721UpgradableBase {
+interface IAirdroppable {
+  event AirdropExecuted(uint256 numMinted);
+
+  function airdrop(AirdropRecipient[] calldata receivers) external;
+}
+
+abstract contract ERC721UpgradableAirdroppable is ERC721UpgradableBase, IAirdroppable {
   // keccak256(abi.encode(uint256(keccak256("rootstock.storage.ERC721Airdroppable")) - 1)) & ~bytes32(uint256(0xff))
   bytes32 private constant storageLocation =
     0xb0b7c350b577073edef3635128030d229b37650766e59f19e264b4b50f30a500;
-
-  event AirdropExecuted(uint256 numMinted);
 
   function _getStorage() private pure returns (AirdroppableStorage storage $) {
     assembly {
@@ -30,12 +34,12 @@ abstract contract ERC721UpgradableAirdroppable is ERC721UpgradableBase {
    *
    * @param receivers an array of receivers with corresponding IPFS CIDS
    */
-  function airdrop(AirdropItem[] calldata receivers) external virtual onlyOwner {
+  function airdrop(AirdropRecipient[] calldata receivers) external virtual override onlyOwner {
     AirdroppableStorage storage $ = _getStorage();
     uint256 tokenId = $._nextTokenId;
     for (uint256 i = 0; i < receivers.length; i++) {
       tokenId++;
-      AirdropItem calldata item = receivers[i];
+      AirdropRecipient calldata item = receivers[i];
       _safeMint(item.receiver, tokenId);
       _setTokenURI(tokenId, item.ipfsCid);
     }
