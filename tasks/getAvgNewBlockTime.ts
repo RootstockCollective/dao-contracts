d246d0e9270cede6dd8aa27515d85562ba9bf1d0
Alexander Evchenko <alexivchenko9@gmail.com>
2024-12-10 14:29:24 +0400

    lint: rules applied

--- tasks/getAvgNewBlockTime.ts
+++ tasks/getAvgNewBlockTime.ts
@@ -1,38 +1,34 @@
-import { task } from "hardhat/config";
+import { task } from 'hardhat/config'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
-task("average-block-time", "Calculates the average time for adding new blocks")
-    .addParam("referenceBlockQuantity", "Number of previous blocks to be considered for the calculation")
-    .setAction(async ({ referenceBlockQuantity }, hre: HardhatRuntimeEnvironment) => {
-        const provider = hre.ethers.provider;
-        const currentBlock = await provider.getBlock("latest");
+task('average-block-time', 'Calculates the average time for adding new blocks')
+  .addParam('referenceBlockQuantity', 'Number of previous blocks to be considered for the calculation')
+  .setAction(async ({ referenceBlockQuantity }, hre: HardhatRuntimeEnvironment) => {
+    const provider = hre.ethers.provider
+    const currentBlock = await provider.getBlock('latest')
 
-        if (!currentBlock) {
-            console.log("Error fetching the current block.");
-            return;
-        }
+    if (!currentBlock) {
+      console.log('Error fetching the current block.')
+      return
+    }
 
-        const referenceQuantity = parseInt(referenceBlockQuantity, 10);
-        if (!currentBlock) {
-            console.log("Error fetching the current block.");
-            return;
-        }
-        const previousBlockNumber = currentBlock.number - referenceQuantity;
+    const referenceQuantity = parseInt(referenceBlockQuantity, 10)
+    const previousBlockNumber = currentBlock.number - referenceQuantity
 
-        if (previousBlockNumber < 0) {
-            console.log("The reference block quantity is too high relative to the current block number.");
-            return;
-        }
+    if (previousBlockNumber < 0) {
+      console.log('The reference block quantity is too high relative to the current block number.')
+      return
+    }
 
-        const previousBlock = await provider.getBlock(previousBlockNumber);
+    const previousBlock = await provider.getBlock(previousBlockNumber)
 
-        if (!previousBlock) {
-            console.log("Error fetching the previous block.");
-            return;
-        }
+    if (!previousBlock) {
+      console.log('Error fetching the previous block.')
+      return
+    }
 
-        const timeDiff = currentBlock.timestamp - previousBlock.timestamp;
-        const averageBlockTime = timeDiff / referenceQuantity;
+    const timeDiff = currentBlock.timestamp - previousBlock.timestamp
+    const averageBlockTime = timeDiff / referenceQuantity
 
-        console.log(`Average time for adding new blocks: ${averageBlockTime.toFixed(2)} seconds`);
-    });
+    console.log(`Average time for adding new blocks: ${averageBlockTime.toFixed(2)} seconds`)
+  })
