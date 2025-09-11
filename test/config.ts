1cca4e7a54906e3542812bf591ecb1f904eefaaf
Cybervoid <mail@cybervoid.my>
2025-09-11 23:01:36 +0200

    refactor: update checkpoints tests to use new import structure and improve address handling

--- test/config.ts
+++ test/config.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import type { Signer } from 'ethers'
 
 const hre = await network.connect()
-const { ethers, ignition } = hre
+const { ethers, ignition, networkHelpers, provider, networkName } = hre
 
 export default hre
-export { expect, type Signer, ethers, ignition }
+export { expect, type Signer, ethers, ignition, networkHelpers, provider, networkName }
