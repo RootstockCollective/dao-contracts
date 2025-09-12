da448e46f1cb5bf2179b343563221ef04cff5ce3
Cybervoid <mail@cybervoid.my>
2025-09-12 23:32:14 +0200

    test: add test file to check husky and lint-staged

---
+++ test-format.ts
@@ -0,0 +1,13 @@
+// Плохо отформатированный файл для тестирования
+const badlyFormatted = {
+  foo: 'bar',
+  baz: 'qux',
+}
+
+function testFunction(param1: string, param2: number) {
+  return {
+    result: param1 + param2,
+  }
+}
+
+export { badlyFormatted, testFunction }
