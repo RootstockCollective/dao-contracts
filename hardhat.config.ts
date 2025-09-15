d81900acf8c0246709c8575d63af654a7d2f69db
Cybervoid <mail@cybervoid.my>
2025-09-16 01:36:38 +0200

    chore: add dotenv dependency and configure environment variables in hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,14 @@ import hardhatToolboxMochaEthersPlugin from '@nomicfoundation/hardhat-toolbox-mo
 import hardhatVerify from '@nomicfoundation/hardhat-verify'
 import { configVariable } from 'hardhat/config'
 import { type HttpNetworkAccountsUserConfig } from 'hardhat/types/config'
+import dotenv from 'dotenv'
 import './tasks/airdrop'
 import './tasks/cancelProposal'
 import './tasks/stRifUpgradeV1-v2'
 import './tasks/updateIpfsFolder'
 import './tasks/withdrawTreasury'
 
+dotenv.config()
 const derivationPath = "m/44'/60'/0'/0"
 const accounts = {
   mnemonic: process.env.MNEMONIC ?? '',
