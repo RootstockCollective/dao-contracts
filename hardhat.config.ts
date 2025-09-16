5162532c935d018f350382d4db5d956d10a88616
Cybervoid <mail@cybervoid.my>
2025-09-16 02:42:09 +0200

    chore: disable Etherscan verification and remove redundant Blockscout configuration in hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -102,22 +102,12 @@ const config: HardhatUserConfig = {
           url: 'https://rootstock-testnet.blockscout.com',
           apiUrl: 'https://rootstock-testnet.blockscout.com/api',
         },
-        etherscan: {
-          name: 'Rootstock Blockscout',
-          url: 'https://rootstock.blockscout.com',
-          apiUrl: 'https://rootstock.blockscout.com/api',
-        },
       },
     },
     30: {
       name: 'rootstockMainnet',
       blockExplorers: {
         blockscout: {
-          name: 'Rootstock Blockscout',
-          url: 'https://rootstock.blockscout.com',
-          apiUrl: 'https://rootstock.blockscout.com/api',
-        },
-        etherscan: {
           name: 'Rootstock Blockscout',
           url: 'https://rootstock.blockscout.com',
           apiUrl: 'https://rootstock.blockscout.com/api',
